4f92886e8b94afbcc88fcc53cb7731f375068c6e
NotRealButter <notrealbutter@gmail.com>
2015-02-16 18:47:28 -0500

    Update and rename myJFrame.java to MyJFrame.java

--- src/pathfindercharactergenerator/MyJFrame.java
+++ src/pathfindercharactergenerator/MyJFrame.java
@@ -2,7 +2,7 @@ package pathfindercharactergenerator;
 
 import javax.swing.*;
 import static javax.swing.JFrame.*;
-public class myJFrame extends JFrame
+public class MyJFrame extends JFrame
 {
 
     int frameWidth = 1005  , frameHeight = 730;
